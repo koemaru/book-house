337d4605789a616bea243c759e4fdc15bcc5b07b
koemaru <yuhaya.1027@gmail.com>
2020-12-14 12:20:45 +0900

    create books-table

---
+++ db/migrate/20201213110711_create_books.rb
@@ -0,0 +1,10 @@
+class CreateBooks < ActiveRecord::Migration[6.0]
+  def change
+    create_table :books do |t|
+      t.string :title,                  null: false
+      t.text :article,                  null: false
+      t.string :image,                  null: false
+      t.timestamps
+    end
+  end
+end
