06f1d699a492a0a68a048d7cdbc3b6c784d447b2
koemaru <yuhaya.1027@gmail.com>
2020-12-21 23:20:05 +0900

    display categories-info-to-top-page

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -3,6 +3,14 @@ class BooksController < ApplicationController
 
   def index
     @new_books = Book.order('created_at DESC')
+    @categories = Category.where(ancestry: nil)
+    @categories_1 = Category.where(ancestry: 1)
+    @categories_2 = Category.where(ancestry: 2)
+    @categories_3 = Category.where(ancestry: 3)
+    @categories_4 = Category.where(ancestry: 4)
+    @categories_5 = Category.where(ancestry: 5)
+    @categories_6 = Category.where(ancestry: 6)
+    @categories_7 = Category.where(ancestry: 7)
   end
 
   def new
@@ -34,7 +42,7 @@ class BooksController < ApplicationController
   end
 
   def book_params
-    params.require(:book).permit(:title, :article, :image)
+    params.require(:book).permit(:title, :article, :image, :category_id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_20_080818) do
+ActiveRecord::Schema.define(version: 2020_12_21_141015) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "title", null: false
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2020_12_20_080818) do
     t.string "image", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "category_id"
   end
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
