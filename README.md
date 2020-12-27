ec990189a2d45b0c50cd168653d02870f302b8a7
koemaru <yuhaya.1027@gmail.com>
2020-12-27 21:17:20 +0900

    create likes-table

--- README.md
+++ README.md
@@ -67,8 +67,8 @@ Things you may want to cover:
 ## likesテーブル
 |Column       |Type         |Options                          |
 |-------------|-------------|---------------------------------|
-|user_id      |references   |null: false, foreign_key: true   |
-|book_id      |references   |null: false, foreign_key: true   |
+|user_id      |integer      |null: false                      |
+|book_id      |integer      |null: false,                     |
 ### Association
 - belongs_to :user
 - belongs_to :book, counter_cache: :likes_count
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_27_115219) do
+ActiveRecord::Schema.define(version: 2020_12_27_121259) do
 
   create_table "books", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "book_title", null: false
@@ -29,6 +29,13 @@ ActiveRecord::Schema.define(version: 2020_12_27_115219) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "ancestry"
     t.index ["ancestry"], name: "index_categories_on_ancestry"
+  end
+
+  create_table "likes", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "book_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
