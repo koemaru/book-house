e8bf14bbba6a060d86b4fef5291dd3ad5fe934a5
koemaru <yuhaya.1027@gmail.com>
2020-12-27 12:29:03 +0900

    Update books_controller.rb
    restrict article-post-page to login-user

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,5 @@
 class BooksController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show, :get_category_children ]
   before_action :set_category, only: [:new, :edit, :create, :update, :destroy]
 
   def index
